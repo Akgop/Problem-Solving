9f83dd19b8d31bd1d9fabe1e1c66eaccbffdf1f3
Akgop <skadbskcd0902@gmail.com>
2020-09-14 13:39:02 +0900

    Create baekjoon_5543.py

---
+++ baekjoon/baekjoon_5543.py
@@ -0,0 +1,16 @@
+burger = 2000
+drink = 2000
+
+for i in range(3):
+    temp = int(input())
+    if temp < burger:
+        burger = temp
+
+for j in range(2):
+    temp = int(input())
+    if temp < drink:
+        drink = temp
+
+result = burger + drink
+result = result - 50
+print(result)
