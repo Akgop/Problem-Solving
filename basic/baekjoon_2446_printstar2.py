110a1335ea34ec61b0baa62e136a46b670a6b6c2
Akgop <skadbsckd0902@gmail.com>
2020-09-17 00:28:06 +0900

    abs, print(+) syntax

---
+++ basic/baekjoon_2446_printstar2.py
@@ -0,0 +1,20 @@
+# My Code
+n = int(input())
+for i in range(n):
+    print(' '*i, end='')
+    print('*'*(2*(n-i)-1))
+for i in range(n-1):
+    print(' '*(n-i-2), end='')
+    print('*'*(2*(i+1)+1))
+
+# Others code 1, range, abs syntax
+num = int(input())
+for n in range(-num+1, num):
+    print(' '*(num-abs(n)-1) + '*'*(2*abs(n)+1))
+
+# Others code 2, print + syntax
+n=int(input())
+for i in range(n):
+    print(" "*i + "*"*(2*n-2*i-1))
+for j in range(n-1):
+    print(" "*(n-j-2) + "*"*(2*j+3))
