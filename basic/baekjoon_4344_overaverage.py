0e64a7b24887ef5f28b33ab3dc19f83bf7149738
Akgop <skadbsckd0902@gmail.com>
2020-11-04 01:00:26 +0900

    Create baekjoon_4344_overaverage.py

---
+++ basic/baekjoon_4344_overaverage.py
@@ -0,0 +1,13 @@
+C = int(input())
+for _ in range(C):
+    score = list(map(int, input().split()))
+    avg = sum(score[1:score[0]+1]) / score[0]
+    cnt = 0
+    for i in range(1, score[0]+1):
+        if score[i] > avg:
+            cnt += 1
+    answer = round((cnt/score[0]), 5)
+    print("%.3f%%" % (answer*100))
+
+
+
