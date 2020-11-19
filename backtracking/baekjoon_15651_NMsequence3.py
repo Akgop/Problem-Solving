fa9c577075b5b571a68b74590002293472f2c1ce
Akgop <skadbsckd0902@gmail.com>
2020-11-20 01:31:15 +0900

    Update baekjoon_15651_NMsequence3.py

--- backtracking/baekjoon_15651_NMsequence3.py
+++ backtracking/baekjoon_15651_NMsequence3.py
@@ -14,3 +14,9 @@ def dfs(depth, n, m):
 N, M = map(int, sys.stdin.readline().split())
 li = []
 dfs(0, N, M)
+
+# itertools 의 product 라이브러리를 사용하는 방법
+#from itertools import product
+
+#n, m = map(int, input().split())
+#print("\n".join(list(map(" ".join, product(map(str, range(1, n+1)), repeat=m)))))
