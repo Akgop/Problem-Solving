748e4e1274425888108024568d58801e23f81536
Akgop <skadbsckd0902@gmail.com>
2021-02-05 23:00:57 +0900

    Update programmers_kakaointern_1.py

--- implementation/simulation/programmers_kakaointern_1.py
+++ implementation/simulation/programmers_kakaointern_1.py
@@ -1,11 +1,15 @@
-def get_manhattan_distance(keypad, num, coord):
-    num_r = 0
-    num_c = 1
+def get_coord(keypad, num):
+    r = 0
+    c = 0
     for i in range(4):
         if num in keypad[i]:
-            num_r = i
-            break
-    return abs(coord[0]-num_c) + abs(coord[1]-num_r)
+            r = i
+            c = keypad[r].index(num)
+    return r, c
+
+
+def get_manhattan_distance(r, c, tar_r, tar_c):
+    return abs(r-tar_r) + abs(c-tar_c)
 
 
 def solution(numbers, hand):
@@ -13,53 +17,41 @@ def solution(numbers, hand):
 
     # 별과 샾은 -1, -2로 초기화, 시작 위치 설정
     keypad = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [-1, 0, -2]]
-    left_hand = [3, 0]
-    right_hand = [3, 2]
+    left_hand = -1
+    right_hand = -2
     
     # 문자열 시작
     for number in numbers:
         # 왼손인 경우
         if number == 1 or number == 4 or number == 7:
             answer += 'L'
-            left_hand = [number // 3, 0]  # 왼손 위치 이동
+            left_hand = number
         # 오른손인 경우
         elif number == 3 or number == 6 or number == 9:
             answer += 'R'
-            right_hand = [(number // 3) - 1, 2]  # 오른손 위치 이동
+            right_hand = number
         # 경합 경우
         else:
-            left_dist = get_manhattan_distance(keypad, number, left_hand)
-            right_dist = get_manhattan_distance(keypad, number, right_hand)
-            # 왼쪽이 더 가까울 경우
+            left_r, left_c = get_coord(keypad, left_hand)
+            right_r, right_c = get_coord(keypad, right_hand)
+            tar_r, tar_c = get_coord(keypad, number)
+            left_dist = get_manhattan_distance(left_r, left_c, tar_r, tar_c)
+            right_dist = get_manhattan_distance(right_r, right_c, tar_r, tar_c)
+            # 왼손이 더 가까우면
             if left_dist < right_dist:
                 answer += 'L'
-                if number == 0:
-                    left_hand = [3, 1]
-                else:
-                    left_hand = [number // 3, 1]
-            # 오른쪽이 더 가까울 경우
+                left_hand = number
+            # 오른손이 더 가까우면
             elif right_dist < left_dist:
                 answer += 'R'
-                if number == 0:
-                    right_hand = [3, 1]
-                else:
-                    right_hand = [number // 3, 1]
-            # 거리가 같은 경우
+                right_hand = number
+            # 양손이 같으면
+            elif hand == "right":
+                answer += 'R'
+                right_hand = number
             else:
-                if hand == "right":
-                    answer += 'R'
-                    if number == 0:
-                        right_hand = [3, 1]
-                    else:
-                        right_hand = [number // 3, 1]
-                else:
-                    answer += 'L'
-                    if number == 0:
-                        left_hand = [3, 1]
-                    else:
-                        left_hand = [number // 3, 1]
-
-                
+                answer += 'L'
+                left_hand = number
 
     return answer
 
