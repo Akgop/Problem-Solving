35c24f1cdf539499e2352c6f61a96f07ca194538
Akgop <skadbsckd0902@gmail.com>
2021-03-18 00:28:33 +0900

    Update thisiscote_greedy_q6.py

--- Greedy/thisiscote_greedy_q6.py
+++ Greedy/thisiscote_greedy_q6.py
@@ -1,50 +1,48 @@
+def get_sum(li, n, mid):
+    answer = 0
+    for i in range(n):
+        if li[i] < mid:
+            answer += li[i]
+        else:
+            answer += mid
+    return answer
+
+
+def binary_search(li, k, start, end, n):
+    answer = 0
+    result_food_ate = 0
+    while start <= end:
+        mid = (start + end) // 2
+        food_ate = get_sum(li, n, mid)
+        if food_ate > k:
+            end = mid - 1
+        else:
+            answer = mid
+            result_food_ate = food_ate
+            start = mid + 1
+    return answer, result_food_ate
+
+
 def solution(food_times, k):
     n = len(food_times)
-    plate = n       # 남은 접시
+    answer, food_ate = binary_search(food_times, k, 0, max(food_times), n)
 
-    overflow = k
-    for f in food_times:
-        overflow -= f
-    if overflow >= 0:
-        return -1
-
-    while True:
-        quo = k // plate        # 남은 접시 row 구함
-        remain = k % plate      # 남은 접시 col 구함
-
-        # 접시 하나 남았으면 그게 답
-        if plate == 1:
-            for i in range(n):
-                if food_times[i] != 0:
-                    return i + 1
-
-        # 먹어야할 접시가 남은 경우 먹는다.
-        tmp_remain = remain + (quo * plate)
-        empty = 0
-        for i in range(n):
-            if food_times[i] < quo:
-                empty += quo - food_times[i]
-                food_times[i] = 0
-            else:
-                food_times[i] -= quo
-            # 만약 먹다가 나머지가 끝나버린다 -> 종료
-            if tmp_remain == 0:
-                return i + 1
-            if food_times[i] != 0:
-                tmp_remain -= 1
-
-        tmp_plate = n
-        for f in food_times:
-            if f == 0:
-                tmp_plate -= 1
-
-        k = empty + remain
-        plate = tmp_plate
+    idx = k - (food_ate - 1)
+    print(k, food_ate, idx, answer)
+    for i in range(n):
+        if food_times[i] <= answer:
+            continue
+        idx -= 1
+        if idx == 0:
+            return i + 1
+    return -1
 
 
 print(
     solution(
-        [4, 1, 3],
-        4
+        [4, 2, 3, 6, 7, 1, 5, 8],
+        16
     )
 )
+# food_times=[4,2,3,6,7,1,5,8] k=16 answer = 3
+# food_times=[4,2,3,6,7,1,5,8] k=27 answer = 5
