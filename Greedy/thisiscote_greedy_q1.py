9f8ab8fe5cf111ea8eeb9b22b7899e156922b031
Akgop <skadbsckd0902@gmail.com>
2021-03-18 14:48:27 +0900

    Update thisiscote_greedy_q1.py

--- Greedy/thisiscote_greedy_q1.py
+++ Greedy/thisiscote_greedy_q1.py
@@ -1,17 +1,32 @@
 import sys
 
+"""
+1번째 풀이 - 틀림
+-> 조건을 전부 확인하지 않음. 몇 명의 모험가는 마을에 그대로 남아있어도 된다.
+"""
+# def solution(n):
+#     li.sort(reverse=True)
+#     count = 0
+#     i = 0
+#     while i < len(li):
+#         i = i + li[i]
+#         count += 1
+#     print(count)
+
 
 def solution(n):
-    li.sort(reverse=True)
+    li.sort()
     count = 0
-    i = 0
-    while i < len(li):
-        i = i + li[i]
+    group = 0
+    for i in range(n):
         count += 1
-    print(count)
+        if li[i] <= count:
+            count = 0
+            group += 1
+    print(group)
 
 
 if __name__ == "__main__":
-    N = int(sys.stdin.readline().rstrip())
-    li = list(map(int, sys.stdin.readline().rstrip().split()))
+    li = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+    N = len(li)
     solution(N)
