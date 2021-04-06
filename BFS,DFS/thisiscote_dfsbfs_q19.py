7173abb3eab2afe97e8737a5526d2b9ac63cf186
Akgop <skadbsckd0902@gmail.com>
2021-04-06 21:16:26 +0900

    Create thisiscote_dfsbfs_q19.py

---
+++ BFS,DFS/thisiscote_dfsbfs_q19.py
@@ -0,0 +1,7 @@
+import sys
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    numbers = list(map(int, sys.stdin.readline().rstrip().split()))
+    operators = list(map(int, sys.stdin.readline().rstrip().split()))
