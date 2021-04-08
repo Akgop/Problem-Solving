e81e87800ad978a2fcc79300bb07a749d5f56a54
Akgop <skadbsckd0902@gmail.com>
2021-04-08 09:52:46 +0900

    14888 DFS와 BFS모두 풀이

--- BFS,DFS/baekjoon_14888_operators.py
+++ BFS,DFS/baekjoon_14888_operators.py
@@ -16,32 +16,39 @@ def operate(num1, num2, cmd):
     return num1
 
 
-def dfs(n, now, idx, op):
+def dfs(n, now, idx):
+    global add, sub, mul, div, min_value, max_value
     # 연산 수행
-    calculated = operate(now, numbers[idx], op)
-
-    # 만약 마지막 인덱스면?
-    if idx == n - 1:    # 최종 결과 저장
-        # 연산 결과 저장
-        pass
-
-    # 4번 탐색하며 operator 에 남아있는 연산을 재귀 수행
-    for i in range(4):
-        # 남아 있으면
-        if operators[i] > 0:
-            # 연산값을 통해 재귀 수행
-            dfs(n, calculated, idx + 1, i)
-
-
-def solution(n):
-    for i in range(4):
-        if operators[i] > 0:
-            # 최종 깊이, 연산 수행한 값, 다음 연산 인덱스, 연산 종류
-            dfs(n, numbers[0], 1, i)
+    if n == idx:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
+        return
+    if add > 0:
+        add -= 1
+        dfs(n, now + numbers[idx], idx + 1)
+        add += 1
+    if sub > 0:
+        sub -= 1
+        dfs(n, now - numbers[idx], idx + 1)
+        sub += 1
+    if mul > 0:
+        mul -= 1
+        dfs(n, now * numbers[idx], idx + 1)
+        mul += 1
+    if div > 0:
+        div -= 1
+        dfs(n, int(now / numbers[idx]), idx + 1)
+        # now // numbers[idx] <- 이렇게 하면 안된다.
+        div += 1
 
 
 if __name__ == "__main__":
     N = int(sys.stdin.readline().rstrip())
     numbers = list(map(int, sys.stdin.readline().rstrip().split()))
-    operators = list(map(int, sys.stdin.readline().rstrip().split()))
-    solution(N)
+    add, sub, mul, div = list(map(int, sys.stdin.readline().rstrip().split()))
+    min_value, max_value = int(1e9), int(-1e9)
+
+    dfs(N, numbers[0], 1)
+
+    print(max_value)
+    print(min_value)
--- BFS,DFS/thisiscote_dfsbfs_q19.py
+++ BFS,DFS/thisiscote_dfsbfs_q19.py
@@ -1,7 +1,48 @@
+""" using BFS """
 import sys
+from collections import deque
+import copy
+
+
+def operate(num1, num2, cmd):
+    if cmd == 0:
+        num1 += num2
+    if cmd == 1:
+        num1 -= num2
+    if cmd == 2:
+        num1 *= num2
+    if cmd == 3:
+        if num1 * num2 < 0:
+            num1 = -(abs(num1) // abs(num2))
+        else:
+            num1 //= num2
+    return num1
+
+
+def bfs(start, op, nxt_idx):
+    q = deque()
+    q.append((start, op, nxt_idx))
+    answer = set()
+    while q:
+        now = q.popleft()
+        # 만약 now[1] 즉, 연산자가 더이상 남지 않았다면? -> 마지막 result 종료조건
+        if sum(now[1]) == 0:
+            answer.add(now[0])
+            continue
+        # 4번 돌며 연산 완탐
+        for i in range(4):
+            # 해당 연산을 할 수 있으면
+            if now[1][i] > 0:
+                # 사칙 연산
+                result = operate(now[0], numbers[now[2]], i)
+                tmp = copy.deepcopy(now[1])
+                tmp[i] -= 1
+                q.append((result, tmp, now[2] + 1))
+    print(max(answer), min(answer))
 
 
 if __name__ == "__main__":
     N = int(sys.stdin.readline().rstrip())
     numbers = list(map(int, sys.stdin.readline().rstrip().split()))
     operators = list(map(int, sys.stdin.readline().rstrip().split()))
+    bfs(numbers[0], operators, 1)
