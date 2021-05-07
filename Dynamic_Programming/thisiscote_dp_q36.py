49cc1b9a4f17a9b93ddf372434172641b3c55565
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-05-07 23:43:10 +0900

    Update thisiscote_dp_q36.py

--- Dynamic_Programming/thisiscote_dp_q36.py
+++ Dynamic_Programming/thisiscote_dp_q36.py
@@ -3,16 +3,24 @@ import sys
 
 # LCS
 def solution(str_a, str_b):
-    table = [[0] * (len(str_b) + 1) for _ in range(len(str_a) + 1)]
+    n = len(str_a)
+    m = len(str_b)
 
-    for i in range(1, len(str_a) + 1):
-        for j in range(1, len(str_b) + 1):
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        dp[i][0] = i
+    for j in range(1, m + 1):
+        dp[0][j] = j
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
             if str_a[i - 1] == str_b[j - 1]:
-                table[i][j] = table[i - 1][j - 1] + 1
+                dp[i][j] = dp[i - 1][j - 1]
             else:
-                table[i][j] = max(table[i - 1][j], table[i][j - 1])
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1])
 
-    return max(len(str_a), len(str_b)) - table[len(str_a)][len(str_b)]
+    return dp[n][m]
 
 
 print(solution("cat", "cut"))
