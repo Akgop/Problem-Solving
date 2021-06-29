fccb82b79e79b843c69b4bdacc9a782888ec294d
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-06-29 11:06:51 +0900

    Update baekjoon_17609_palindrome.py

--- Greedy/baekjoon_17609_palindrome.py
+++ Greedy/baekjoon_17609_palindrome.py
@@ -1,28 +1,37 @@
 import sys
 
 
-def solution(string):
-    flag = 0
-    start, end = 0, len(string) - 1
-    while end >= start:
-        # 앞 뒤가 같으면 pass
+def is_palindrome(string, start, end):
+    while start <= end:
         if string[start] == string[end]:
             start, end = start + 1, end - 1
             continue
-        # 앞 뒤가 다르면!
-        flag += 1
-        if string[start + 1] == string[end]:
-            start += 1
-        elif string[end - 1] == string[start]:
-            end -= 1
         else:
-            flag = 2
-        if flag == 2:
-            break
-    return flag
+            return start, end
+    return start, end
+
+
+def is_pseudo(string, start, end):
+    start, end = is_palindrome(string, start, end)
+    if start > end:
+        return 1
+    else:
+        return 2
+
+
+def solution(string):
+    start, end = is_palindrome(string, 0, len(string) - 1)
+    if start > end:     # 팰린드롬인 경우
+        return 0
+    else:
+        result = is_pseudo(string, start + 1, end)
+        if result == 2:
+            result = is_pseudo(string, start, end - 1)
+        return result
 
 
 if __name__ == "__main__":
+    sys.setrecursionlimit(100000)
     T = int(sys.stdin.readline().rstrip())
     for _ in range(T):
         _str = sys.stdin.readline().rstrip()
