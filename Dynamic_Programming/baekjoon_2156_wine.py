f7e63d690a5ee287b152604f4b059ae74f107e7d
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-07-06 16:30:39 +0900

    Update baekjoon_2156_wine.py

--- Dynamic_Programming/baekjoon_2156_wine.py
+++ Dynamic_Programming/baekjoon_2156_wine.py
@@ -1,28 +1,30 @@
 import sys
-N = int(input())
-v = [0]*N
-for i in range(N):
-    v[i] = (int(sys.stdin.readline()))
-if N == 1:
-    print(v[0])
-elif N == 2:
-    print(v[0]+v[1])
-elif N == 3:
-    tmp = [0] * 3
-    tmp[0] = v[0] + v[1]
-    tmp[1] = v[1] + v[2]
-    tmp[2] = v[0] + v[2]
-    print(max(tmp))
-else:
-    result = [0] * N
-    tmp = [0] * 3
-    tmp[0] = v[0] + v[1]
-    tmp[1] = v[1] + v[2]
-    tmp[2] = v[0] + v[2]
-    result[0] = v[0]
-    result[1] = v[0] + v[1]
-    result[2] = max(tmp)
-    for i in range(3, N):
-        result[i] = max(v[i-1] + result[i-3], result[i-2]) + v[i]
-        result[i] = max(result[i-1], result[i])
-    print(result[-1])
+
+# 다른 사람이 짠 dp 코드. 엄청 깔끔한데 어떻게 이런 생각을 할 수 있을지 잘 모르겠다..
+# def solution(n):
+#     a, b, c = 0, 0, 0
+#     for w in wine:
+#         a, b, c = max(a, b, c), a + w, b + w
+#     return max(a, b, c)
+
+
+def solution(n):
+    if n < 3:
+        return sum(wine)
+    result = [0] * n
+    result[0] = wine[0]
+    result[1] = wine[0] + wine[1]
+    result[2] = max(wine[0] + wine[1], wine[0] + wine[2], wine[1] + wine[2])
+    for i in range(3, n):
+        result[i] = max(
+            result[i-3] + wine[i-1] + wine[i],
+            result[i-2] + wine[i],
+            result[i-1]
+        )
+    return result[-1]
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    wine = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+    print(solution(N))
