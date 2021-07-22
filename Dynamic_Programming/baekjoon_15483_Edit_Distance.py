215a4eddd56af55d8b1be3dbcb3d22947b10097d
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-07-23 00:37:39 +0900

    Update baekjoon_15483_Edit_Distance.py

--- Dynamic_Programming/baekjoon_15483_Edit_Distance.py
+++ Dynamic_Programming/baekjoon_15483_Edit_Distance.py
@@ -1,18 +1,19 @@
 import sys
 
 
-# LCS
+# Minimum Edit
 def solution(str_a, str_b):
-    table = [[0] * (len(str_b) + 1) for _ in range(len(str_a) + 1)]
-
-    for i in range(1, len(str_a) + 1):
-        for j in range(1, len(str_b) + 1):
-            if str_a[i - 1] == str_b[j - 1]:
-                table[i][j] = table[i - 1][j - 1] + 1
-            else:
-                table[i][j] = max(table[i - 1][j], table[i][j - 1])
-
-    return max(len(str_a), len(str_b)) - table[len(str_a)][len(str_b)]
+    table = [[0] * (len(str_a)+1) for _ in range(len(str_b)+1)]
+    table[0] = [i for i in range(len(str_a)+1)]
+    for i in range(len(str_b)+1):
+        table[i][0] = i
+    for i in range(1, len(str_b)+1):
+        for j in range(1, len(str_a)+1):
+            if str_a[j-1] == str_b[i-1]:
+                table[i][j] = table[i-1][j-1]
+                continue
+            table[i][j] = min(table[i-1][j], table[i][j-1], table[i-1][j-1]) + 1
+    return table[-1][-1]
 
 
 if __name__ == "__main__":
