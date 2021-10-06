c5545d57f8c62a252002727599c4d0de41a9fb81
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-10-07 02:33:18 +0900

    BFS 1, DFS 1

--- Backtracking/baekjoon_14888_insert_operator.py
+++ Backtracking/baekjoon_14888_insert_operator.py
@@ -1,38 +1,36 @@
-def cal(n):
-    answer = A[0]
-    for i in range(n-1):
-        if result[i] == 0:
-            answer = answer + A[i + 1]
-        elif result[i] == 1:
-            answer = answer - A[i + 1]
-        elif result[i] == 2:
-            answer = answer * A[i + 1]
-        else:
-            if answer < 0:
-                answer = 0 - (abs(answer) // A[i + 1])
-            else:
-                answer = answer // A[i + 1]
-    return answer
+import sys
 
 
-def dfs(depth, n):
-    if depth == n-1:
-        ans.append(cal(n))
+def cal(op, a, b):
+    if op == 0:
+        return a + b
+    elif op == 1:
+        return a - b
+    elif op == 2:
+        return a * b
+    else:
+        return -(-a // b) if a < 0 else a // b
+
+
+def recur(max_depth, depth, result):
+    global MAX, MIN
+    if depth == max_depth:
+        MAX = max(result, MAX)
+        MIN = min(result, MIN)
         return
-    for j in range(4):
-        if Op[j] != 0:
-            Op[j] -= 1
-            result.append(j)
-            dfs(depth+1, n)
-            result.pop()
-            Op[j] += 1
+
+    for i in range(len(ops)):
+        if ops[i] != 0:
+            ops[i] -= 1
+            recur(max_depth, depth+1, cal(i, result, seq[depth]))
+            ops[i] += 1
 
 
-N = int(input())
-A = list(map(int, input().split()))
-Op = list(map(int, input().split()))
-result = []
-ans = []
-dfs(0, N)
-print(max(ans), min(ans))
-
+if __name__ == "__main__":
+    MAX, MIN = -int(1e9), int(1e9)
+    N = int(sys.stdin.readline())
+    seq = list(map(int, sys.stdin.readline().split()))
+    ops = list(map(int, sys.stdin.readline().split()))
+    recur(N, 1, seq[0])
+    print(MAX)
+    print(MIN)
